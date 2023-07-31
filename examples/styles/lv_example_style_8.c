93cce36cbfeeb9a051de17f937b71300f9a520a7
999613 <80238779+999613@users.noreply.github.com>
2023-07-31 17:51:10 +0800

    Update lv_example_style_8.c

--- examples/styles/lv_example_style_8.c
+++ examples/styles/lv_example_style_8.c
@@ -14,12 +14,12 @@ void lv_example_style_8(void)
     lv_style_set_bg_color(&style, lv_palette_lighten(LV_PALETTE_GREY, 2));
     lv_style_set_border_width(&style, 2);
     lv_style_set_border_color(&style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_pad_all(&style, 10);
+    lv_style_set_pad_all(&style, 10);    设置内边距
 
-    lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_text_letter_space(&style, 5);
-    lv_style_set_text_line_space(&style, 20);
-    lv_style_set_text_decor(&style, LV_TEXT_DECOR_UNDERLINE);
+    lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_BLUE));    文本颜色
+    lv_style_set_text_letter_space(&style, 5);        文本字母间距
+    lv_style_set_text_line_space(&style, 20);        文本行间距
+    lv_style_set_text_decor(&style, LV_TEXT_DECOR_UNDERLINE);    设置下划线
 
     /*Create an object with the new style*/
     lv_obj_t * obj = lv_label_create(lv_scr_act());
