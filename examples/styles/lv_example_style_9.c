09d5e53e236be00eff7b95bbda9d1d25ca444eab
999613 <80238779+999613@users.noreply.github.com>
2023-07-31 17:55:27 +0800

    Update lv_example_style_9.c

--- examples/styles/lv_example_style_9.c
+++ examples/styles/lv_example_style_9.c
@@ -11,16 +11,16 @@ void lv_example_style_9(void)
 
     lv_style_set_line_color(&style, lv_palette_main(LV_PALETTE_GREY));
     lv_style_set_line_width(&style, 6);
-    lv_style_set_line_rounded(&style, true);
+    lv_style_set_line_rounded(&style, true);        线条的拐弯处是否做圆角处理
 
     /*Create an object with the new style*/
     lv_obj_t * obj = lv_line_create(lv_scr_act());
     lv_obj_add_style(obj, &style, 0);
 
     static lv_point_t p[] = {{10, 30}, {30, 50}, {100, 0}};
-    lv_line_set_points(obj, p, 3);
+    lv_line_set_points(obj, p, 3);    点坐标的位置
 
-    lv_obj_center(obj);
+    lv_obj_center(obj);    显示在屏幕中间
 }
 
 #endif
