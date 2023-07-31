ca2e5b1ffb716182c572b748328dc9f1294de533
999613 <80238779+999613@users.noreply.github.com>
2023-07-31 16:59:03 +0800

    Update lv_example_style_1.c
    增加注释

--- examples/styles/lv_example_style_1.c
+++ examples/styles/lv_example_style_1.c
@@ -8,16 +8,16 @@ void lv_example_style_1(void)
 {
     static lv_style_t style;
     lv_style_init(&style);
-    lv_style_set_radius(&style, 5);
+    lv_style_set_radius(&style, 5);        //设置圆角 半径为5
 
     /*Make a gradient*/
     lv_style_set_width(&style, 150);
-    lv_style_set_height(&style, LV_SIZE_CONTENT);
+    lv_style_set_height(&style, LV_SIZE_CONTENT);    //设置宽高
 
-    lv_style_set_pad_ver(&style, 20);
-    lv_style_set_pad_left(&style, 5);
+    lv_style_set_pad_ver(&style, 20);            //设置对象的垂直内边距属性
+    lv_style_set_pad_left(&style, 5);            //设置对象的左内边距属性
 
-    lv_style_set_x(&style, lv_pct(50));
+    lv_style_set_x(&style, lv_pct(50));        //设置x坐标为屏幕的50%
     lv_style_set_y(&style, 80);
 
     /*Create an object with the new style*/
